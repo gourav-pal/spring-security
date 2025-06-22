7c4c8ef1988d93b5d3f67c35c4d683e78da41b59
gourav-pal <Palgourav04@gmail.com>
2025-06-22 15:36:52 +0530

    refactor: remove default CSRF protection and implement custom security configuration

--- src/main/java/com/example/spring_security/ByPass.java
+++ src/main/java/com/example/spring_security/ByPass.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class ByPass {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-//        return http.build();
-//    }
-//}
+
+@Configuration
+public class ByPass {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        return http.build();
+    }
+}
